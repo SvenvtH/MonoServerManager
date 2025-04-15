a2b9fcf9ef485b0e9e82b6c58b59402bcdfa718a
Sven van 't Hoenderdaal <sven@hoenderdaal.net>
2025-04-15 18:06:06 +0200

    Initial commit

---
+++ MonoServerManager/Core.cs
@@ -0,0 +1,85 @@
+﻿using MelonLoader;
+using ScheduleOne.PlayerScripts;
+using HarmonyLib;
+using Steamworks;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using FishNet.Example.ColliderRollbacks;
+using ScheduleOne.Networking;
+using FishySteamworks;
+using MonoServerManager.GameServermanager;
+
+[assembly: MelonInfo(typeof(MonoServerManager.Core), "MonoServerManager", "1.0.0", "Red", null)]
+[assembly: MelonGame("TVGS", "Schedule I")]
+
+namespace MonoServerManager
+{
+    public class Core : MelonMod
+    {
+
+        private MelonPreferences_Category serverSettings;
+        private MelonPreferences_Entry<ELobbyType> lobbyTypeSetting;
+        private MelonPreferences_Entry<int> maxPlayerCount;
+        internal static GameServerManager? GSManager;
+
+        enum LobbyType
+        {
+            Public,
+            FriendsOnly,
+            Private
+        }
+
+        public void Awake()
+        {
+            SteamAPI.Init();
+        }
+        public override void OnInitializeMelon()
+        {
+            serverSettings = MelonPreferences.CreateCategory("serverSettings");
+            lobbyTypeSetting = serverSettings.CreateEntry<ELobbyType>("lobbyTypeSetting", ELobbyType.k_ELobbyTypePublic);
+            maxPlayerCount = serverSettings.CreateEntry<int>("maxPlayerCount", 4);
+
+
+        }
+
+
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+        {
+            if (sceneName == "Menu")
+            {
+                if(SteamManager.Initialized)
+                {
+                    GSManager = new GameServerManager();
+
+                }
+            }
+        }
+
+
+        public void CreateLobby()
+        {
+            MelonLogger.Msg("Creating lobby with settings: " + lobbyTypeSetting.Value + " and max players: " + maxPlayerCount.Value);
+            ELobbyType lobbyType = lobbyTypeSetting.Value;
+            int maxPlayers = maxPlayerCount.Value;
+            SteamMatchmaking.CreateLobby(lobbyType, maxPlayers);
+        }
+
+        // Remove Friend check from PlayerNameData RPC method
+        [HarmonyPatch(typeof(Player))]
+        private class PlayerPatches
+        { 
+            [HarmonyPatch(nameof(Player.RpcLogic___SendPlayerNameData_586648380))]
+            [HarmonyPrefix]
+            private static bool RpcLogic___SendPlayerNameData_586648380_Prefix(Player __instance, string playerName, ulong id)
+            {
+                __instance.ReceivePlayerNameData(null, playerName, id.ToString());
+                __instance.PlayerName = playerName;
+                __instance.PlayerCode = id.ToString();
+                return false;
+            }
+        }
+
+
+    }
+}
---
+++ MonoServerManager/GameServerManager.cs
@@ -0,0 +1,85 @@
+﻿using MelonLoader;
+using ScheduleOne.UI.MainMenu;
+using Steamworks;
+using UnityEngine;
+
+namespace MonoServerManager.GameServermanager;
+public class GameServerManager
+{
+    public readonly bool IsInit;
+    public bool IsRunning;
+
+    private const int GAMEPLAY_PORT = 27015;
+    private const int QUERY_PORT = 27016;
+
+    public GameServerManager()
+    {
+        if (!GameServer.Init(0, GAMEPLAY_PORT, QUERY_PORT, EServerMode.eServerModeNoAuthentication, Application.version))
+        {
+            MelonLogger.Error("Game server failed to initialise.");
+            if (MainMenuPopup.InstanceExists)
+                MainMenuPopup.Instance.Open("Error", "Game server failed to initialise.", true);
+
+            return;
+        }
+
+        MelonLogger.Msg($"Game server created for ports :{QUERY_PORT} and :{GAMEPLAY_PORT}.");
+        IsInit = true;
+
+        Callback<SteamServersConnected_t>.CreateGameServer(_ =>
+        {
+            IsRunning = true;
+
+            MelonLogger.Warning("Game server is now connected to Steam servers.");
+            if (MainMenuPopup.InstanceExists)
+                MainMenuPopup.Instance.Open("Info", "Game server is now connected to Steam servers.", false);
+
+            SteamGameServer.SetAdvertiseServerActive(true);
+        });
+        Callback<SteamServerConnectFailure_t>.CreateGameServer(f =>
+        {
+            MelonLogger.Error("Failed to login to Steam servers: " + f.m_eResult);
+            if (MainMenuPopup.InstanceExists)
+                MainMenuPopup.Instance.Open("Error", $"Failed to login to Steam servers, did you supply the right token? ({f.m_eResult.ToString()})", true);
+        });
+        Callback<SteamServersDisconnected_t>.CreateGameServer(f =>
+        {
+            IsRunning = false;
+
+            MelonLogger.Error("GS got disconnected from Steam servers: " + f.m_eResult);
+            if (MainMenuPopup.InstanceExists)
+                MainMenuPopup.Instance.Open("Error", $"Game server was disconnected from Steam servers: {f.m_eResult.ToString()}", true);
+        });
+
+        AppId_t gameAppId = new AppId_t(3164500);
+        SteamApps.GetAppInstallDir(gameAppId, out var installDir, 2048);
+        MelonLogger.Msg($"App install dir: {installDir}");
+
+        if (!Directory.Exists(installDir))
+        {
+            return; // could be a lil pirate, let's block 'em hehe
+        }
+
+        SteamGameServer.SetProduct("Schedule I");
+        SteamGameServer.SetGameDescription("Schedule I");
+
+        SteamGameServer.SetModDir(
+            "s1_dedicated_server"
+        );
+
+        UpdateInfo();
+        MelonLogger.Msg($"Logging in to Steam Game Server...");
+        SteamGameServer.LogOn("STEAM_GAME_SERVER_ID");
+    }
+
+    public void UpdateInfo()
+    {
+        SteamGameServer.SetServerName("Test Server");
+        SteamGameServer.SetMapName("Main Map");
+
+        SteamGameServer.SetMaxPlayerCount(Math.Min(Math.Max(4, 1), 128));
+
+        SteamGameServer.SetPasswordProtected(false);
+        SteamGameServer.SetDedicatedServer(true);
+    }
+}
